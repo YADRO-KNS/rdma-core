aae48434ae46c73e4855fd7ff884248e60859d5e
Alexander Barabash <Alexander.Barabash@emc.com>
2019-11-02 01:00:20 +0200

    NTRDMA_IOCTL_SEND

--- providers/ntrdma/dev.c
+++ providers/ntrdma/dev.c
@@ -36,7 +36,121 @@
 #include <string.h>
 #include <unistd.h>
 #include <malloc.h>
+#include <sys/ioctl.h>
 #include "ntrdma.h"
+
+/* MUST BE THE SAME AS IN KERNEL */
+struct ntrdma_send_wqe {
+	uint64_t			ulp_handle;
+	uint16_t			op_code;
+	uint16_t			op_status;
+	uint32_t			recv_key;
+	struct ibv_sge			rdma_sge;
+	uint32_t			imm_data;
+	uint32_t			flags;
+	union {
+		uint32_t		sg_count;
+		uint32_t		inline_len;
+	};
+};
+
+/* MUST BE THE SAME AS IN KERNEL */
+struct ntrdma_wr_snd_sge {
+	uint32_t			key;
+	union {
+		/* key != NTRDMA_RESERVED_DMA_LEKY */
+		struct {
+			uint64_t	addr;
+			uint32_t	len;
+		};
+		/* key == NTRDMA_RESERVED_DMA_LEKY */
+		/* struct ntc_local_buf	snd_dma_buf; */
+		uint8_t			filler[24];
+	};
+};
+
+/* MUST BE THE SAME AS IN KERNEL */
+struct ntrdma_snd_hdr {
+	uint32_t			wqe_counter;
+	uint32_t			first_wqe_size;
+};
+
+/* MUST BE THE SAME AS IN KERNEL */
+#define NTRDMA_IOCTL_BASE 'N'
+
+/* MUST BE THE SAME AS IN KERNEL */
+#define NTRDMA_IOCTL_SEND		_IOWR(NTRDMA_IOCTL_BASE, 0x30, uint32_t)
+
+static inline int make_ntrdma_send_wqe(struct ntrdma_send_wqe *wqe,
+				struct ibv_send_wr *swr,
+				int available_size)
+{
+	int available_size_in = available_size;
+	bool is_inline;
+	size_t tail_size;
+	void *ptr;
+	struct ntrdma_wr_snd_sge *ntrdma_sge;
+	struct ibv_sge *ibv_sge;
+	size_t len;
+	int i;
+
+	static_assert(sizeof(struct ibv_sge) == sizeof(swr->sg_list[0]),
+		"Expecting struct ibv_sge in swr->sg_list");
+
+	if (available_size < (int)sizeof(*wqe))
+		return -ENOMEM;
+	available_size -= sizeof(*wqe);
+
+	is_inline = ((swr->opcode == IBV_WR_RDMA_WRITE) &&
+		(swr->send_flags & IBV_SEND_INLINE));
+
+	if (is_inline) {
+		for ((i = 0), (tail_size = 0); i < swr->num_sge; i++) {
+			tail_size += swr->sg_list[i].length;
+			if (available_size < tail_size)
+				return -ENOMEM;
+		}
+		available_size -= tail_size;
+	} else {
+		tail_size = swr->num_sge * sizeof(struct ntrdma_wr_snd_sge);
+		if (available_size < tail_size)
+			return -ENOMEM;
+		available_size -= tail_size;
+	}
+
+	wqe->ulp_handle = swr->wr_id;
+	wqe->op_code = swr->opcode;
+	wqe->rdma_sge.addr = swr->wr.rdma.remote_addr;
+	wqe->rdma_sge.lkey = swr->wr.rdma.rkey;
+	wqe->imm_data = swr->imm_data;
+	wqe->flags = swr->send_flags;
+	if (is_inline)
+		wqe->inline_len = tail_size;
+	else
+		wqe->sg_count = swr->num_sge;
+
+	if (!tail_size)
+		return available_size_in - available_size;
+
+	if (is_inline)
+		for ((i = 0), (ptr = wqe + 1); i < swr->num_sge;
+		     (i++), (ptr += len)) {
+			len = swr->sg_list[i].length;
+			if (!len)
+				continue;
+			memcpy(ptr, (void *)swr->sg_list[i].addr, len);
+		}
+	else
+		for ((i = 0), (ntrdma_sge = (void *)(wqe + 1)),
+			     (ibv_sge = swr->sg_list);
+		     i < swr->num_sge; (i++), (ntrdma_sge++), (ibv_sge++)) {
+			ntrdma_sge->key = ibv_sge->lkey;
+			ntrdma_sge->addr = ibv_sge->addr;
+			ntrdma_sge->len = ibv_sge->length;
+		}
+
+	return available_size_in - available_size;
+}
 
 int ntrdma_query_device(struct ibv_context *context,
 			struct ibv_device_attr *device_attr)
@@ -277,17 +391,64 @@ int ntrdma_query_qp(struct ibv_qp *qp,
 int ntrdma_post_send(struct ibv_qp *_qp, struct ibv_send_wr *swr,
 		     struct ibv_send_wr **bad)
 {
-	struct ntrdma_qp *qp = to_ntrdma_qp(_qp);
-	int rc;
-
 	DEFINE_NTC_FUNC_PERF_TRACKER(perf, 1 << 20);
+	struct ntrdma_qp *qp = to_ntrdma_qp(_qp);
+	int offset;
+	struct ntrdma_send_wqe *wqe;
+	struct ntrdma_snd_hdr *hdr = qp->buffer;
+	uint32_t *wqehdr;
+	int wqe_counter;
+	struct ibv_send_wr *swr_first;
+	int rc = 0;
+
+	if (qp->fd < 0) {
+		rc = ibv_cmd_post_send(_qp, swr, bad);
+		NTC_PERF_MEASURE(perf);
+		return rc;
+	}
 
 	pthread_mutex_lock(&qp->mutex);
 
-	/* Now use qp->buffer */
+	while (swr) {
+		swr_first = swr;
 
-	rc = ibv_cmd_post_send(_qp, swr, bad);
+		wqehdr = &hdr->first_wqe_size;
+		for ((wqe_counter = 0), (offset = sizeof(*hdr));
+		     swr; (swr = swr->next), (wqe_counter++)) {
+			wqe = qp->buffer + offset;
+			rc = make_ntrdma_send_wqe(wqe, swr,
+						qp->buffer_size - offset);
+			if (rc < 0)
+				break;
+			*wqehdr = rc;
+			offset += rc;
+			wqehdr = &wqe->recv_key;
+		}
+
+		if (!wqe_counter)
+			break;
+
+		if (unlikely(swr))
+			PRINT_DEBUG_KMSG("NTRDMADEB: %s: LONG SEND LIST. "
+					"Sending %d WRs in one ioctl\n",
+					__func__, wqe_counter);
+
+		hdr->wqe_counter = wqe_counter;
+
+		rc = ioctl(qp->fd, NTRDMA_IOCTL_SEND, qp->buffer);
+
+		if (rc >= 0)
+			continue;
+
+		for ((wqe_counter = hdr->wqe_counter), (swr = swr_first);
+		     wqe_counter; wqe_counter--)
+			swr = swr->next;
+		break;
+	}
+
 	pthread_mutex_unlock(&qp->mutex);
+
+	*bad = swr;
 
 	NTC_PERF_MEASURE(perf);
 
